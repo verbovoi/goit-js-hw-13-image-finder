7f5d3c33ed2611a72f6e2006808baf4d7b44c11e
Eugene Verbovoi <63497261+verbovoi@users.noreply.github.com>
2020-11-11 22:41:47 +0200

    Добавляет бесконечный скрол

--- src/apiService.js
+++ src/apiService.js
@@ -1,8 +1,37 @@
 const API_KEY = '19060641-7ffe1969b41865fad4803ab0d';
 const BASE_URL = 'https://pixabay.com/api';
-let page = 1;
 
-export default function fetchImages(image) {
-    return fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${image}&page=${page}&per_page=12&key=${API_KEY}`).
-        then(response => response.json());
+// export default function fetchImages(image) {
+//     return fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${image}&page=${page}&per_page=12&key=${API_KEY}`).
+//         then(response => response.json());
+// }
+
+
+export default class ImageApi{
+    constructor(){
+        this.searchQuery = '';
+        this.page = 1;
+        }
+
+    fetchImages() {
+        return fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`).
+            then(response => response.json());
+    }
+
+    incrementPage() {
+        this.page += 1; 
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }     
+            
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
 //Импорты
 import './styles.css';
-import fetchImages from './apiService';
+import ImageApi from './apiService';
 import imageCardTpl from './templates/image-card.hbs'
 
 
 //Элементы ДОМ
 const refs = {
     searchFormEl: document.querySelector('.search-form'),
-    galleryListEl: document.querySelector('.gallery')
+    galleryListEl: document.querySelector('.gallery'),
+    // searchForm: document.querySelector('.js-search-form'),
+    galleryContainerEl: document.querySelector('.gallery-container'),
+    sentinel: document.querySelector('#sentinel'),
 }
+
+const imageApi = new ImageApi();
 
 refs.searchFormEl.addEventListener('submit', onSearchImage);
 
-function onSearchImage(event) { 
-    event.preventDefault();
-    const form = event.currentTarget;
-    const searchImage = form.elements.query.value;
-    console.log(searchImage);
 
+function onSearchImage(e) { 
+    e.preventDefault();
 
-    fetchImages(searchImage).
-    then(onRenderGallery).
-    catch(onCatchError).
-    finally(() => form.reset());
+    imageApi.query = e.currentTarget.elements.query.value;
 
+    if (imageApi.query === '') {
+        return alert('Enter search key');
+    }
 
+    imageApi.resetPage();
+    clearArticlesContainer();
+    imageApi.fetchImages().then(onRenderGallery).catch(onCatchError);
+    
 }
 
 function onRenderGallery(gallery) {
     const markup = imageCardTpl(gallery);
-    console.log(markup);
     refs.galleryListEl.innerHTML = markup;
-    
+    imageApi.incrementPage();   
+}
+
+function appendImagesMarkup(images) {
+    refs.galleryListEl.insertAdjacentHTML('beforeend', imageCardTpl(images));
+        imageApi.incrementPage();   
+
+}
+
+function clearArticlesContainer() {
+  refs.galleryListEl.innerHTML = '';
 }
 
 function onCatchError(error) {
     console.log(error);
 }
+
+
+//Функция бесконечного скролла
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && imageApi.query !== '') {
+      // console.log('Пора грузить еще статьи' + Date.now());
+      imageApi.fetchImages().
+    then(appendImagesMarkup)
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '200px',
+});
+observer.observe(refs.sentinel);
